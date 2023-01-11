15eb60d8b25426d89b7c8fd9b4d8a8b0271d2433
ivan retamozo <retamozoi445@gmail.com>
2023-01-11 18:02:32 -0300

    rutas de generos

--- build/utils/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleHttp = void 0;
-const handleHttp = (res, error, errRaw) => {
-    console.log(errRaw);
-    res.status(400).json({ error });
-};
-exports.handleHttp = handleHttp;
